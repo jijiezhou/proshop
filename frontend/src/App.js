090346f7f0d26563924d1479037f64c2a022e27c
jijiezhou <jijiez@email.unc.edu>
2023-10-03 22:39:34 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-10-03 22:27:46
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-10-03 22:32:35
+ */
+const App = () => {
+  return <h1>Welcome to Proshop</h1>;
+};
+
+export default App;
