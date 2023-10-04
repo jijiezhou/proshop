01d11c632eab44e78c3ed4e7eca8643cb2e1719f
jijiezhou <jijiez@email.unc.edu>
2023-10-03 23:17:09 -0400

    header & footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,24 @@
  * @Author: ZJJ
  * @Date: 2023-10-03 22:27:46
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-10-03 22:32:35
+ * @LastEditTime: 2023-10-03 23:10:34
  */
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Welcome to Proshop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="text-center py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,36 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-10-03 22:45:42
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-10-03 22:58:33
+ */
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser />
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
