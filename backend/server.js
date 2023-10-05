11ff2dc7675ef55272ba35e0b80a672832a0e45e
jijiezhou <jijiez@email.unc.edu>
2023-10-04 23:36:42 -0400

    express server

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-10-04 23:17:50
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-10-04 23:35:52
+ */
+import express from "express";
+import products from "./data/products.js";
+//const express = require('express')
+
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`Server running on ${port}`));
