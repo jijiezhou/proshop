0ec426f958d3dfb99d7ee7134776cc2b1a960796
jijiezhou <jijiez@email.unc.edu>
2023-10-04 22:47:16 -0400

    product details page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@
  * @Author: ZJJ
  * @Date: 2023-10-03 22:27:46
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-10-04 00:58:38
+ * @LastEditTime: 2023-10-04 22:24:30
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -18,11 +18,13 @@ import {
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,86 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-10-04 22:21:44
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-10-04 22:43:05
+ */
+import { useParams } from "react-router-dom";
+import React from "react";
+import products from "../products";
+import { Link } from "react-router-dom";
+import { ListGroup } from "react-bootstrap";
+import { Row, Col, Image, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+
+const ProductScreen = () => {
+  const { id: productId } = useParams();
+  const product = products.find((p) => p._id === productId);
+  console.log(product);
+
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Descripttion: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
