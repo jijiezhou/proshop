c19c64afb50a5d1321ecc1c51896dd141d8583e0
jijiezhou <jijiez@email.unc.edu>
2023-10-07 14:54:35 -0400

    cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@
  * @Author: ZJJ
  * @Date: 2023-10-03 22:27:46
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-10-06 22:51:29
+ * @LastEditTime: 2023-10-07 14:29:02
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -21,12 +21,14 @@ import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
