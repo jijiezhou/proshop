edcc7b3f932ef2589bfbb292345f793d7c7ae32e
jijiezhou <jijiez@email.unc.edu>
2023-10-07 16:41:17 -0400

    jwt http only cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@
  * @Author: ZJJ
  * @Date: 2023-10-07 15:14:04
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-10-07 15:53:35
+ * @LastEditTime: 2023-10-07 16:35:07
  */
 /*
  * @Descripttion: ZJJ Code
@@ -16,6 +16,7 @@
  */
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 // @desc Auth user & get token
 // @route POST /api/users/login
@@ -25,6 +26,18 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email: email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    //Set JWT as HTTP-only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
